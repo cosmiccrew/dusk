b1ac5769584d919afafb54445559541fc07cc5d9
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2023-06-06 18:13:01 +0100

    add prelude/useful consts

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+pub mod prelude {
+    pub use std::f32::consts::*;
+
+    pub use anyhow::{anyhow, bail, ensure, Result};
+
+    pub use crate::{MODELS, MODELS_DIR};
+}
+
+pub const MODELS: &'static str = "spacekit/models/";
+pub const MODELS_DIR: &str = concat!("assets/", "spacekit/models/");
